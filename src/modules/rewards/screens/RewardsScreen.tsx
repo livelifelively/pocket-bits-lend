2cd0a9762ae3096555abf2ef15dbaa65520c184a
Ashish Rana <dev.livelifelively@gmail.com>
2021-04-09 19:59:26 +0530

    reward screen init

--- src/modules/rewards/screens/RewardsScreen.tsx
+++ src/modules/rewards/screens/RewardsScreen.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 
-const RewardScreen = () => {
-  return <Text style={styles.text}>Reward</Text>;
+import { DefaultLayout } from "../../../layouts/Default";
+import { RewardsNavProps } from "../RewardsParamList";
+
+const RewardScreen = ({navigation}: RewardsNavProps<"Rewards">) => {
+  return (
+    <DefaultLayout>
+      <Text style={styles.text}>Reward Screen</Text>
+    </DefaultLayout>
+  )
 };
 
 const styles = StyleSheet.create({
