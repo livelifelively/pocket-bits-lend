4d1d1da67e3a64248ac15bdcfb108f47d7115eb0
Ashish Rana <dev.livelifelively@gmail.com>
2021-05-06 14:08:57 +0530

    get user data from Async storage

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,19 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ColorSchemeName } from 'react-native';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 
-import { AuthContext } from '../modules/auth/AuthProvider';
 import { AuthStack } from '../modules/auth/AuthStack';
 import { PasscodeAuthStack } from '../modules/passcode-auth/PasscodeAuthStack';
 import { SetPasscodeStack } from '../modules/set-passcode/SetPasscodeStack';
 import { AppStack } from './AppStack';
 import Logger from '../services/logger';
-import { User } from '../types';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
-  const { user } = useContext(AuthContext);
+  const [user, setUser] = useState<User>(() => null);
 
-  Logger.info('NAVIGATION', user);
+  useEffect(() => {
+    const getUser = async () => {
+      const loggedInUser = await AsyncStorage.getItem('user');
+      if (loggedInUser) {
+        const loggedInUserObject: User = JSON.parse(loggedInUser);
+        Logger.info('NAVIGATION', loggedInUserObject);
+        setUser(loggedInUserObject);
+      } else {
+        setUser(null);
+      }
+    };
+    getUser();
+  }, []);
 
   return (
     <NavigationContainer theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
