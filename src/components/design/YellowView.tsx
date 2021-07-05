45c2280d842def83e657fcc06e34c58d0fdf5c95
Ashish Rana <dev.livelifelively@gmail.com>
2021-07-05 06:49:29 +0530

    yellow gradient issue fix

--- src/components/design/YellowView.tsx
+++ src/components/design/YellowView.tsx
@@ -1,3 +1,4 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
@@ -6,12 +7,20 @@ interface YellowViewProps {
 }
 
 export const YellowView: React.FC<YellowViewProps> = ({ children, style }) => {
-  return <View style={{ ...styles.yellowView, ...style }}>{children}</View>;
+  const linearGradientColors = [];
+  linearGradientColors.push('rgba(255, 223, 92, 1)');
+  linearGradientColors.push('rgba(255, 184, 80, 0.94)');
+
+  return (
+    <LinearGradient colors={linearGradientColors} style={[styles.yellowView, style]}>
+      <View>{children}</View>
+    </LinearGradient>
+  );
 };
 
 const styles = StyleSheet.create({
   yellowView: {
-    backgroundColor: '#FFB850',
+    // backgroundColor: '#FFB850',
     padding: 15,
     borderRadius: 15,
     width: '100%',
